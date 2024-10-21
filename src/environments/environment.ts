af145fce352be40be978f17316664c75647e5120
Scurvybelt <90409656+Scurvybelt@users.noreply.github.com>
2024-10-21 15:03:16 -0600

    servicios para conectar con BackEnd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiLocal: 'http://localhost/apiAixa/servicesController.php'
 };
 
 /*
