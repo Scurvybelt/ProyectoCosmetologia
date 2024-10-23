bc5f258b5a394a34bf06d19e6ad89f0709683b38
Scurvybelt <90409656+Scurvybelt@users.noreply.github.com>
2024-10-23 16:24:28 -0600

    Fotos de Apartado nosotros y arreglar navbar

--- src/app/components/pages/contact-page/contact-page.component.ts
+++ src/app/components/pages/contact-page/contact-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-page',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-page.component.scss']
 })
 export class ContactPageComponent implements OnInit {
+  formCorreo: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { 
+    this.formCorreo = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]],
+      tel: ['', [Validators.required]],
+      asunto: ['', [Validators.required]]
+    });
+  }
 
   ngOnInit(): void {
+    
+  }
+
+  mandarFomrulario(){
+    console.log(this.formCorreo.value);
+    
   }
 
 }
