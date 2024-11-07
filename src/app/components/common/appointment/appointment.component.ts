2086e9eee421c0bb64595c6cee248e13f97d4f55
Ray Mondragon <raymondragonn@gmail.com>
2024-11-07 15:19:42 -0600

    link de redirección Whatsapp dentro de 'Haz Una Cita'

--- src/app/components/common/appointment/appointment.component.ts
+++ src/app/components/common/appointment/appointment.component.ts
@@ -14,4 +14,54 @@ export class AppointmentComponent implements OnInit {
 
     ngOnInit(): void {}
 
+    agendarCita(): void {
+        const nombre = this.removeAccents((document.getElementById('nombre') as HTMLInputElement).value);
+        const correo = (document.getElementById('correo') as HTMLInputElement).value;
+        const celular = (document.getElementById('celular') as HTMLInputElement).value;
+        const fecha = (document.getElementById('fecha') as HTMLInputElement).value;
+        const mensaje = this.removeAccents((document.getElementById('mensaje') as HTMLTextAreaElement).value);
+
+        if (!nombre) {
+            alert('Por favor, ingrese su nombre.');
+            return;
+        }
+
+        if (!correo) {
+            alert('Por favor, ingrese su correo.');
+            return;
+        }
+
+        if (!correo.includes('@')) {
+            alert('Por favor, ingrese un correo válido.');
+            return;
+        }
+
+        if (!celular) {
+            alert('Por favor, ingrese su celular.');
+            return;
+        }
+
+        if (!/^\d{1,10}$/.test(celular)) {
+            alert('Por favor, ingrese un número de celular válido (solo números y no más de 10 caracteres).');
+            return;
+        }
+
+        if (!mensaje) {
+            alert('Por favor, ingrese su mensaje.');
+            return;
+        }
+
+        if (mensaje.length > 100) {
+            alert('El mensaje no debe exceder los 100 caracteres.');
+            return;
+        }
+
+        const url = `https://api.whatsapp.com/send/?phone=2299012681&text=Hola!👋🏼+Quiero%20agendar%20una%20cita⏰%0ANombre: ${nombre}%0ACorreo: ${correo}%0ACelular: ${celular}%0AFecha: ${fecha}%0AMensaje: ${mensaje}`;
+        
+        window.location.href = url;
+    }
+
+    removeAccents(str: string): string {
+        return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    }
 }
